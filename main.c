c487640d454290b98235457dcd63964b66a66c8f
Gielsson <gielssonsilva045@gmail.com>
2026-05-21 00:20:15 -0300

    Update main.c

--- main.c
+++ main.c
@@ -10,9 +10,12 @@ int mnenuPrincipal(){
 int opção; // variavel usada para guardar a opção escolhida  pelo usuário do menu
 while(opção <1 || opção > 3){
 printf("Escolha uma opção: ");
-printf("\n 1 - tarara 1");
-printf("\n 2 - terere 2");
-printf("\n 3 - tiriri 3\n");
+printf("\n 1 - Gerenciar Livros");
+printf("\n 2 - Gerenciar Usuários");
+printf("\n 3 - Realizar Empréstimo \n");
+printf("\n 4 - Registrar Devolução");
+printf("\n 5 - Relatórios");
+printf("\n 6 - Sair \n");
 scanf("%d", &opção);
 //resultado de acordo com a opção escolhida.
 swuitch(opção){
@@ -24,6 +27,15 @@ swuitch(opção){
     break;
     case 3:
     printf("opção 3 foi escolhida\n");
+    break;
+    case 4:
+    printf("opção 4 foi escolhida\n");
+    break;
+    case 5:
+    printf("opção 5 foi escolhida\n");
+    break;
+    case 6:
+    printf("opção 6 foi escolhida\n");
     break;
     default:
     printf("opção invalida\n");
