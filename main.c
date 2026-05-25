c63d2425e186282766417779a83c43cf07452d04
Gielsson <gielssonsilva045@gmail.com>
2026-05-25 16:33:30 -0300

    Refactor livro and usuario structures and add functions

--- main.c
+++ main.c
@@ -5,12 +5,12 @@
 
 struct livro{
     int codigo;
-    char titulo[50];
-    char autor[50];
-    int ano;
+    char titulo[250];
+    char autor[250];
+    int ano_de_publi;
     char genero[50];
-    int qtd_total[100];
-    int qtd_disponivel;
+    int qtd_total;
+    int quant_disp;
    int total_emprestimos;
 };
 typedef struct livro livro;
@@ -29,25 +29,62 @@ struct emprestimo{
      char data_retirada[11];
      char data_prevista[11];
     char data_devolucao[11];
-    int devolvido;/// ideia: 0 para não devolvido, 1 para devolvido
+    int devolvido;/// Ex: 0 para não devolvido, 1 para devolvido
 }; typedef struct emprestimo emprestimo;
 
-void menuPrincipal(){
+void desenhaBorda(){ 
+     printf("📖========================📖 \n");//função que desenha borda
+
+}
+void limpaTela(){ // FUNÇÃO QUE LIMPA A TELA
+    system ("CLS || clear");// se for linux =cls, se wind = clear
+}
+void cadastrarLivro(livro lista[], int posicao, int id_automatico) {
+    
+    // O sistema define automaticamente o código do livro na posição atual
+    lista[posicao].codigo = id_automatico;
+    printf("--- CADASTRANDO LIVRO (Codigo: %d) ---\n", id_automatico);
+    printf("Digite o titulo do livro: \n");
+    scanf(" %[^\n]s", lista[posicao].titulo); //%[^\n]s lê até apertar Enter
+    printf("Digite o autor do livro: \n");
+    scanf(" %[^\n]s", lista[posicao].autor);
+    printf("Digite o ano de publicacao do livro: \n");
+    scanf("%d", &lista[posicao].ano_de_publi);
+    printf("Digite o genero do livro: \n");
+    scanf(" %[^\n]s", lista[posicao].genero);
+    printf("Digite a quantidade de livros disponiveis: \n");
+    scanf("%d", &lista[posicao].quant_disp);
+    printf("\nLivro cadastrado com sucesso!\n");
+}
+int main(){
+setlocale(LC_ALL, "Portuguese");
+livro lista_livros[100]; //espaço que guarda ate 100 fichas de livros
+int total_livros = 0;    // Cont de livros cadastrados (começa em 0)
+int proximo_codigo = 1;  // guarda o  próximo id (começa em 1)
 int opcao; // variavel usada para guardar a opção escolhida  pelo usuário do menu
 do{
-    printf("\n=====ACERVO DE LIVROS=====\n");
-printf("\n 1 - Gerenciar Livros ");
-printf("\n 2 - Gerenciar Usuários");
-printf("\n 3 - Realizar Empréstimo ");
-printf("\n 4 - Registrar Devolução ");
-printf("\n 5 - Relatórios \n");
-printf("\n 6 - Sair \n");
+     limpaTela();
+    desenhaBorda();
+    printf("\n📚     ACERVO DE LIVROS   📚\n");
+    desenhaBorda();
+printf("\n [1] -📘 Gerenciar Livros ");
+printf("\n [2] -👥 Gerenciar Usuários");
+printf("\n [3] -🤝 Realizar Empréstimo ");
+printf("\n [4] -↩️ Registrar Devolução ");
+printf("\n [5] -📊 Relatórios \n");
+printf(" [6] -❌ Sair \n");
 printf("Escolha uma opção: ");
-scanf("%d", &opção);
+scanf("%d", &opcao);
 //resultado de acordo com a opção escolhida.
-switch(opção){
+switch(opcao){
     case 1:
-    printf("opção 1 foi escolhida\n");
+    if (total_livros < 100) {
+       cadastrarLivro(lista_livros, total_livros, proximo_codigo);
+     total_livros++;    // Avança para o próxima espaço do armário
+    proximo_codigo++;  // Aumenta o número do ID para o próximo livro
+   } else {
+     printf("Erro: Limite de 100 livros atingido!\n");
+}
     break;
     case 2:
     printf("opção 2 foi escolhida\n");
@@ -69,22 +106,6 @@ switch(opção){
     break;
 }
 }while (opcao != 6); // O programa continua rodando ATÉ o usuário digitar 6
-
-}
-void cadastrarLivro //função bagunçada
-int n, i;
-printf("digite a quantidade de exemplare: \n");
-scanf("%d", &n);
-livro quant[n];
-for(i=0; i<n; i++){
-    printf("digite o codigo do novo livro: ")
-}
-int main(){
-setlocale(LC_ALL, "Portuguese");
-
-
-
-menuPrincipal();
 
 return 0;
 }
